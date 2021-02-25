20a9939f7312f0b182a352c2b5d70571a02aa828
Pulkit Nagpal <nagpalpulkit321@gmail.com>
2021-02-25 13:19:37 +0530

    Created Basic Layout

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>social-networking-site</title>
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<!-- Navbar section below -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">GenZ</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">About Us</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<body>
+<!-- <div class="row">
+		<div class="col-sm-12">
+			<div class="well">
+				<center><h1 style="color: white;">Social Networking Site</h1></center>
+			</div>
+		</div>
+	</div> -->
+
 <?php
-echo "Test";
+include 'includes/header.php';
 ?>
+<div class="m-4">
+	<div class="row">	
+		<div class="col-md-7">
+			<img src="assets/images/mainpic.jpg" class="img-fluid mt-auto" title="Connect">
+			
+		</div>
+		<div class="col-md-5">
+			<img src="assets/images/logo.png" class="img-fluid w-75" title="Business Icon">
+			<h2><strong>See what's happening in <br> the world right now</strong></h2><br><br>
+			<h4><strong>join Us Today.</strong></h4>
+			<form>
+				<button id="signup" class="btn btn-info btn-lg w-75" name="signup">Sign up</button><br><br>
+				<button id="login" class="btn btn-info btn-lg w-75" name="login">Login</button><br><br>
+			</form>
+		</div>
+	</div>
+
+</div>
+	
+<?php
+include 'includes/footer.php';
+?>
+
+
+
+
+
+
+</body>
+
+
+</html>
